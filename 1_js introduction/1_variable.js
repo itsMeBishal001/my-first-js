a528d0e59a002ae9ea32eba673ba1ed3c508dba8
itsMeBishal001 <dattabishal001@gmail.com>
2021-12-18 17:32:04 +0530

    my first day

--- 1_js introduction/1_variable.js
+++ 1_js introduction/1_variable.js
@@ -14,3 +14,17 @@ if(num){
     console.log("num is prime")
 }else
  console.log("num is not prime")
+ //diff between var and let--
+ var a=6
+ console.log(a)
+ var a=8
+ console.log(a)
+ var a = "bishal"
+ console.log(a)
+ var a= "rik"
+ console.log(a)
+ let a="rik"
+ let a="bishal"
+ console.log(a)
+ // so this is the main difference between var and let --using var you can decleare a variable many time but using let you can declear
+ // variable one time but you can assign the value next time
--- 1_js introduction/2_function.js
+++ 1_js introduction/2_function.js
@@ -2,3 +2,8 @@ function multiply(a,b){
     console.log(a*b)
 }
 multiply(3,3)
+//iife function-- imediately invoked function expression
+var multiply=(function(a,b){
+    return a*b
+})(2,3)
+console.log(multiply)
