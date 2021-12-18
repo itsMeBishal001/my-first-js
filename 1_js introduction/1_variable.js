7c39a9250e6049fa9b8f39878bf11444f7b82eee
itsMeBishal001 <dattabishal001@gmail.com>
2021-12-18 17:02:20 +0530

    init

---
+++ 1_js introduction/1_variable.js
@@ -0,0 +1,16 @@
+var a
+console.log(a)
+a="its me bishal"
+console.log(a)
+var a= 13
+var num=true
+for(var i=2;i*i<=a;i++){
+    if(a%i==0){
+        num=false
+        break
+    }
+}
+if(num){
+    console.log("num is prime")
+}else
+ console.log("num is not prime")
